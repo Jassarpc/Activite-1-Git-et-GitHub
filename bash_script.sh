540edc64f619871beb1c20fec804d71dffcd740b
Jassarpc <yasserhamidullah@gmail.com>
2016-01-06 00:53:39 +0300

    Ajout de vérification du deuxième l'argument

--- bash_script.sh
+++ bash_script.sh
@@ -11,3 +11,12 @@ then
   echo "le fichier $1 n'a pas été trouvé, vérifiez que le nom est correct"
   exit
 fi
+# si le second paramètre est utilisé, on récupère l'option demandée
+if [ ! -z $2 ]
+then
+  case "$2" in
+    -r|--reverse) sort="sort -n -k 1" ;;
+    -l|--letter) sort="sort -k 3" ;;
+    *)
+      echo "L'option $2 n'est pas disponible"
+      exit
